30ed779399f616fbfc0798acf31f795bd20b91d2
Nishanth Selvaraju <nishanth@activevertex.co.in>
2025-09-26 07:33:16 +0530

    LazyVerticalGrid

--- app/src/main/java/com/nishanth/simplecomposeui/MainActivity.kt
+++ app/src/main/java/com/nishanth/simplecomposeui/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SimpleComposeApp()
+            ProductGrid()
         }
 
     }
