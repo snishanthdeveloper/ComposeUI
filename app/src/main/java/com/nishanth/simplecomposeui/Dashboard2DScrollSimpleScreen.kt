a719857cdaf2fc8c88b1cebf86deffdaa6b05dc1
Nishanth Selvaraju <nishanth@activevertex.co.in>
2025-10-28 08:50:52 +0530

    Some update

--- app/src/main/java/com/nishanth/simplecomposeui/Dashboard2DScrollSimpleScreen.kt
+++ app/src/main/java/com/nishanth/simplecomposeui/Dashboard2DScrollSimpleScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
--- app/src/main/java/com/nishanth/simplecomposeui/model/DashboardCard.kt
+++ app/src/main/java/com/nishanth/simplecomposeui/model/DashboardCard.kt
@@ -2,7 +2,6 @@ package com.nishanth.simplecomposeui.model
 
 import androidx.compose.ui.graphics.Color
 
-// models/DashboardCard.kt
 data class DashboardCard(
     val title: String,
     val value: String,
